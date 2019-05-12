7cb8ebb34a880e447aea2c12d8e1c6e4329a9ba6
Patrick Maurits Sangian <patricksangian@gmail.com>
2019-05-13 04:39:02 +0700

    fix code

--- oop_basic3/main.go
+++ oop_basic3/main.go
@@ -1,7 +1,12 @@
 package main
 
 import (
+	"encoding/json"
+	"fmt"
+
+	"github.com/sangianpatrick/ss-golang/oop_basic3/modules/employee"
 	employeeDomain "github.com/sangianpatrick/ss-golang/oop_basic3/modules/employee/domain"
+	"github.com/sangianpatrick/ss-golang/oop_basic3/modules/employee/model"
 	employeeRepository "github.com/sangianpatrick/ss-golang/oop_basic3/modules/employee/repository"
 	mgo "gopkg.in/mgo.v2"
 )
@@ -32,10 +37,63 @@ func main() {
 
 	//--------------------------------------------------------------------------
 	// Go to employee handler function (CRUD)
-	// List of function is written in handler.go
 	//--------------------------------------------------------------------------
-
-	// createEmployee(eD)
+	// var person = model.Employee{
+	// 	ID:        "EMP0002",
+	// 	FirstName: "Patrick",
+	// 	LastName:  "Sangian",
+	// 	Email:     "patricksangian@email.com",
+	// 	Phone:     "093459345354",
+	// 	Address:   "Jl. Karet Karya 1 No. 5",
+	// 	Position:  "Backend Developer",
+	// }
+	// createEmployee(person, eD)
+	// printEmployeeByID("EMP0001", eD)
 	printAllEmployee(eD)
+}
 
+//----------------------------------------------------------------------------
+// List of Handler function
+//----------------------------------------------------------------------------
+
+func createEmployee(employee model.Employee, d employee.Domain) {
+	err := d.CreateOne(employee)
+	if err != nil {
+		fmt.Println(err)
+	} else {
+		fmt.Println("New Employee has been successfully added")
+	}
+}
+
+func printEmployeeByID(id string, d employee.Domain) {
+	employee, err := d.GetByID(id)
+	if err != nil {
+		fmt.Println(err)
+	} else {
+		employeeByte, _ := json.Marshal(employee)
+		employeeJSON := string(employeeByte)
+		fmt.Printf("Print Employee By ID: \n%v\n", employeeJSON)
+	}
+}
+
+func printEmployeeByEmail(email string, d employee.Domain) {
+	employee, err := d.GetByEmail(email)
+	if err != nil {
+		fmt.Println(err)
+	} else {
+		employeeByte, _ := json.Marshal(employee)
+		employeeJSON := string(employeeByte)
+		fmt.Printf("Print Employee By Email: \n%v\n", employeeJSON)
+	}
+}
+
+func printAllEmployee(d employee.Domain) {
+	employees, err := d.GetAll()
+	if err != nil {
+		fmt.Println(err)
+	} else {
+		employeesByte, _ := json.Marshal(employees)
+		employeesJSON := string(employeesByte)
+		fmt.Printf("Print Employee By ID: \n%v\n", employeesJSON)
+	}
 }
